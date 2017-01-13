5a3036228970dc4d57ca70e19920b9e35201f972
Joseph Woodward <joseph.woodward@xeuse.com>
2017-01-13 09:14:13 +0000

    More clean up

--- src/vscodeilviewer/src/extension.ts
+++ src/vscodeilviewer/src/extension.ts
@@ -8,7 +8,7 @@ import * as os from 'os';
 
 var request = require('request');
 
-let previewUri = vscode.Uri.parse('il-viewer://authority/' + IntermediateLanguageContentProvider.Scheme);
+let ilWindowUri = vscode.Uri.parse('il-viewer://authority/' + IntermediateLanguageContentProvider.Scheme);
 
 let getServiceUrl = () => {
     return vscode.workspace.getConfiguration("ilViewer")["serviceUrl"];
@@ -19,14 +19,14 @@ export function activate(context: vscode.ExtensionContext) {
     // let res = vscode.workspace.textDocuments;
 
     let invokationDisposable = vscode.commands.registerCommand('editor.showIlWindow', () => {
-        return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two, 'IL Viewer').then((success) => {
+        return vscode.commands.executeCommand('vscode.previewHtml', ilWindowUri, vscode.ViewColumn.Two, 'IL Viewer').then((success) => {
         }, (reason) => {
             vscode.window.showErrorMessage(reason);
         });
     });
     registerDisposable(invokationDisposable);    
 
-    let provider = new IntermediateLanguageContentProvider(previewUri);
+    let provider = new IntermediateLanguageContentProvider(ilWindowUri);
     let providerDisposable = vscode.workspace.registerTextDocumentContentProvider(IntermediateLanguageContentProvider.Scheme, provider);
     registerDisposable(providerDisposable);
 
